e1862e0bd205e4ab12d0a673a36b1e8f9cae919c
tarikyesilcimenn <tarikyesilcimenn@gmail.com>
2023-12-03 22:13:21 +0300

    Create layout modal
    Create layout modal

--- prepare-quiz/src/app/quiz/page.tsx
+++ prepare-quiz/src/app/quiz/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react'
+import { Modal, Button, Divider } from 'antd'
 
 const Quiz = () => {
+
+  const [showModal, setShowModal] = useState<boolean>()
+  const [selectedLayout, setSelectedLayout] = useState<string>('')
+
+  useEffect(() => {
+    setShowModal(true)
+  },[])
+
+
+
   return (
-    <div>QUİZ PAGEE</div>
+    <div className={showModal === true ? 'blur-sm' : ''}>
+      <div className='flex justify-center items-center bg-white h-[74px] text-black'>
+        <h1>LOGO</h1>
+      </div>
+      <div className='flex justify-center'>
+        <div className='h-[1357px] w-[1025px] bg-white my-5'>
+          <p>asd</p>
+          <Divider type="vertical" />
+          <p>asd</p>
+        </div>
+
+      </div>
+      {showModal &&
+      <Modal open={showModal} footer={false} closable={false}>
+        <p className='text-xl'>Hello teacher, Please select your quiz layout 😇</p>
+        <div className='flex justify-center items-center gap-10'>
+            <img onClick={() => setSelectedLayout('single')} src='https://png.pngtree.com/png-vector/20200224/ourmid/pngtree-colorful-loading-icon-png-image_2152960.jpg' width='100' height='200' className={selectedLayout === 'single' ? 'border border-green-400' : '' }/>
+            <img onClick={() => setSelectedLayout('double')} src='https://png.pngtree.com/png-vector/20200224/ourmid/pngtree-colorful-loading-icon-png-image_2152960.jpg' width='100' height='200' className={selectedLayout === 'double' ? 'border border-green-400' : '' }/>
+        </div>
+        <div className='flex justify-end'>
+          <Button onClick={() => setShowModal(false)} disabled={selectedLayout.length === 0} >Select</Button>
+
+        </div>
+      </Modal>
+      }
+    </div>
   )
 }
 
